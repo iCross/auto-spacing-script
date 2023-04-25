1807359f902bd40b3330f4a349fe8f5c9256795a
Lukas <rja@icross.cc>
2023-04-25 12:23:35 +0800

    refactor

--- index.js
+++ index.js
@@ -1,83 +1,103 @@
 // ==UserScript==
 // @name         Auto add space between CJK and English
 // @namespace    pangu-userscript
-// @version      1.2.0
+// @version      1.2.2
 // @license      MIT
 // @description  Automatically add spaces between CJK (Chinese, Japanese, and Korean) characters and English letters, taking into account situations like code blocks and dynamic DOM updates.
 // @match        http*://*/*
 // @grant        none
 // @homepageURL  https://github.com/iCross/auto-spacing-script
-// @doownloadURL https://raw.github.comusercontent/iCross/auto-spacing-script/main/index.js
+// @doownloadURL https://raw.githubusercontent.com/iCross/auto-spacing-script/main/index.js
 // @exclude      https://jsfiddle.net/*
 // @require      https://unpkg.com/pangu@4.0.7/dist/browser/pangu.min.js
 // @require      https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
 // ==/UserScript==
-(function () {
-  "use strict";
-  const { register } = VM.shortcut;
 
-  register("c-e", () => {
+(function() {
+    "use strict";
+
+    // Register a keyboard shortcut (c-e) using Violentmonkey's
+    const { register } = VM.shortcut;
+    register("c-u", () => {
+        // Run once when the script is loaded
+        spacingFunction();
+        console.log("You just pressed Shortcut in violetmonkey.");
+    });
+
+    /**
+     * Adds spacing between CJK and English characters
+     */
+    function spacingFunction() {
+        const elements = document.querySelectorAll(
+            'body *:not(script):not(style):not(noscript):not(pre):not(code):not(input):not([contenteditable="true"])'
+        );
+        Array.from(elements).forEach((element) => {
+            // Only space text nodes
+            if (
+                element.childNodes.length === 1 &&
+                element.childNodes[0].nodeType === Node.TEXT_NODE
+            ) {
+                const originalText = element.textContent;
+                const newText = pangu.spacing(originalText);
+                if (originalText !== newText) {
+                    element.textContent = newText; // Update text content
+                }
+            }
+        });
+    }
+
+    /**
+     * Start observing the DOM for changes
+     */
+    function observeDOM(observer) {
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    }
+
+    /**
+     * Debounce utility function
+     */
+    function debounce(func, wait, immediate) {
+        let timeout;
+        return function() {
+            const context = this;
+            const args = arguments;
+            const later = function() {
+                timeout = null;
+                if (!immediate) {
+                    func.apply(context, args);
+                }
+            };
+            const callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) {
+                func.apply(context, args);
+            }
+        };
+    }
+
+    const spacingFunctionDebounced = debounce(spacingFunction, 300);
+
     // Run once when the script is loaded
-    addSpacing();
-    console.log("You just pressed Shortcut in violetmonkey.");
-  });
+    spacingFunction();
 
-  function addSpacing() {
-    const elements = document.querySelectorAll(
-      'body *:not(script):not(style):not(noscript):not(pre):not(code):not(input):not([contenteditable="true"]'
-    );
-    elements.forEach((element) => {
-      if (
-        element.childNodes.length === 1 &&
-        element.childNodes[0].nodeType === Node.TEXT_NODE
-      ) {
-        const originalText = element.textContent;
-        const newText = pangu.spacing(originalText);
-        if (originalText !== newText) {
-          element.textContent = newText;
-        }
-      }
+    // Observe DOM changes
+    const observer = new MutationObserver(() => {
+        requestAnimationFrame(() => {
+            spacingFunctionDebounced();
+        });
     });
-  }
 
-  function observeDOM(observer) {
-    observer.observe(document.body, {
-      childList: true,
-      subtree: true,
-    });
-  }
-
-  function debounce(func, wait, immediate) {
-    let timeout;
-    return function () {
-      const context = this,
-        args = arguments;
-      const later = function () {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  }
-
-  const addSpacingDebounced = debounce(addSpacing, 300);
-
-  // Run every time the content of the page is changed
-  const observer = new MutationObserver(() => {
-    requestAnimationFrame(() => {
-      addSpacingDebounced();
-    });
-  });
-
-  // Start observing when the DOM is ready
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", () => {
-      observeDOM(observer);
-    });
-  } else {
-    observeDOM(observer);
-  }
+    // Start observing
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", () => {
+            observeDOM(observer);
+        });
+    } else {
+        observeDOM(observer);
+    }
 })();
+
