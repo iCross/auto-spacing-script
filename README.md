ecb3b40cacbaf0a625e6fa813aa0c91634d91ffa
Lukas <rja@icross.cc>
2023-04-25 09:22:12 +0800

    fea: add shortcut

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# 自动添加空格 - 油猴脚本
+# 自動添加空格 - 油猴腳本
 
-> 脚本由 [ChatGPT GPT-4 模型](https://chat.openai.com/chat) 编写，可能存在 BUG。
+> 腳本由 [ChatGPT GPT-4 模型](https://chat.openai.com/chat) 編寫，可能存在 BUG。
 
-使用 [pangu.js](https://github.com/vinta/pangu.js/blob/master/dist/browser/pangu.js) 在 CJK （中日繁）字符和英文字母之间自动添加空格，考虑了代码块、DOM 动态更新等情况。
+使用 [pangu.js](https://github.com/vinta/pangu.js/blob/master/dist/browser/pangu.js) 在 CJK （中日繁）字符和英文字母之間自動添加空格，考慮了代碼塊、DOM 動態更新等情況。
 
-## 测试例子
+## 測試例子
 
-这是一行没有在demo的两边添加空格的文本。(如果你安装了脚本，将会显示空格)
+這是一行沒有在demo的兩邊添加空格的文本。(如果你安裝了腳本，將會顯示空格)
 
-这是一行在 demo 的两边添加了空格的文本。
+這是一行在 demo 的兩邊添加了空格的文本。
--- index.js
+++ index.js
@@ -1,70 +1,80 @@
 // ==UserScript==
-// @name         自动添加空格，在 CJK （中日繁）字符和英文字母之间自动添加空格
+// @name         Auto add space between CJK and English
 // @namespace    pangu-userscript
 // @version      1.2.0
 // @license      MIT
-// @description  在 CJK （中日繁）字符和英文字母之间自动添加空格，考虑了代码块、DOM 动态更新等情况。
+// @description  Automatically add spaces between CJK (Chinese, Japanese, and Korean) characters and English letters, taking into account situations like code blocks and dynamic DOM updates.
 // @match        http*://*/*
 // @grant        none
 // @require      https://unpkg.com/pangu@4.0.7/dist/browser/pangu.min.js
+// @require      https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
 // ==/UserScript==
+(function () {
+  "use strict";
+  const { register } = VM.shortcut;
 
-;(function () {
-  'use strict'
+  register("c-e", () => {
+    // Run once when the script is loaded
+    addSpacing();
+    console.log("You just pressed Shortcut in violetmonkey.");
+  });
 
   function addSpacing() {
     const elements = document.querySelectorAll(
       'body *:not(script):not(style):not(noscript):not(pre):not(code):not(input):not([contenteditable="true"]'
-    )
-    elements.forEach(element => {
-      if (element.childNodes.length === 1 && element.childNodes[0].nodeType === Node.TEXT_NODE) {
-        const originalText = element.textContent
-        const newText = pangu.spacing(originalText)
+    );
+    elements.forEach((element) => {
+      if (
+        element.childNodes.length === 1 &&
+        element.childNodes[0].nodeType === Node.TEXT_NODE
+      ) {
+        const originalText = element.textContent;
+        const newText = pangu.spacing(originalText);
         if (originalText !== newText) {
-          element.textContent = newText
+          element.textContent = newText;
         }
       }
-    })
+    });
   }
 
   function observeDOM(observer) {
-    observer.observe(document.body, { childList: true, subtree: true })
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
   }
 
   function debounce(func, wait, immediate) {
-    let timeout
+    let timeout;
     return function () {
       const context = this,
-        args = arguments
+        args = arguments;
       const later = function () {
-        timeout = null
-        if (!immediate) func.apply(context, args)
-      }
-      const callNow = immediate && !timeout
-      clearTimeout(timeout)
-      timeout = setTimeout(later, wait)
-      if (callNow) func.apply(context, args)
-    }
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
   }
 
-  const addSpacingDebounced = debounce(addSpacing, 300)
-
-  // Run once when the script is loaded
-  addSpacing()
+  const addSpacingDebounced = debounce(addSpacing, 300);
 
   // Run every time the content of the page is changed
   const observer = new MutationObserver(() => {
     requestAnimationFrame(() => {
-      addSpacingDebounced()
-    })
-  })
+      addSpacingDebounced();
+    });
+  });
 
   // Start observing when the DOM is ready
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', () => {
-      observeDOM(observer)
-    })
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", () => {
+      observeDOM(observer);
+    });
   } else {
-    observeDOM(observer)
+    observeDOM(observer);
   }
-})()
+})();
