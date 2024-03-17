e90a1b63dae0dd3137ef173469d726efb904dc5a
Lukas <rja@icross.cc>
2024-03-17 23:17:14 +0800

    feat: add autoExecuteUrlsinREGEX feature and merge commits from the original forked source.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto add space between CJK and English
 // @namespace    pangu-userscript
-// @version      1.2.2
+// @version      1.2.3
 // @license      MIT
 // @description  Automatically add spaces between CJK (Chinese, Japanese, and Korean) characters and English letters, taking into account situations like code blocks and dynamic DOM updates.
 // @match        http*://*/*
@@ -12,95 +12,136 @@
 // @exclude      https://www.notion.so/*
 // @require      https://unpkg.com/pangu@4.0.7/dist/browser/pangu.min.js
 // @require      https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
+// @grant        GM_getValue
+// @grant        GM_setValue
+
 // ==/UserScript==
+
+const ignores = ['SCRIPT', 'STYLE', 'NOSCRIPT', 'PRE', 'CODE', 'INPUT']
+
+const isIgnore = name => ignores.includes(name)
+const isEditable = el => el.getAttribute('contenteditable')?.toLowerCase() === 'true'
+const isMonaco = el => el.getAttribute('class')?.includes("monaco-editor")
+
+function acceptNode(node) {
+    const isAccept = isIgnore(node.tagName) || isEditable(node) || isMonaco(node)
+    return isAccept ? NodeFilter.FILTER_REJECT : NodeFilter.FILTER_ACCEPT
+}
+
+/**
+ * Adds spacing between CJK and English characters
+ */
+function spacingFunction() {
+    const walker = document.createTreeWalker(
+        document.body,
+        NodeFilter.SHOW_ELEMENT,
+        { acceptNode },
+        false
+    )
+
+    const elements = []
+
+    while (walker.nextNode()) {
+        elements.push(walker.currentNode)
+    }
+    Array.from(elements).forEach((element) => {
+        // Only space text nodes
+        if (
+            element.childNodes.length === 1 &&
+            element.childNodes[0].nodeType === Node.TEXT_NODE
+        ) {
+            const originalText = element.textContent
+            const newText = pangu.spacing(originalText)
+            if (originalText !== newText) {
+                element.textContent = newText;
+            }
+        }
+    });
+}
+
+/**
+ * Start observing the DOM for changes
+ */
+function observeDOM(observer) {
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
+}
+
+/**
+ * Debounce utility function
+ */
+function debounce(func, wait, immediate) {
+    let timeout;
+    return function() {
+        const context = this;
+        const args = arguments;
+        const later = function() {
+            timeout = null;
+            if (!immediate) {
+                func.apply(context, args);
+            }
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) {
+            func.apply(context, args);
+        }
+    };
+}
+
+const spacingFunctionDebounced = debounce(spacingFunction, 300);
+
+// Run once when the script is loaded
+//spacingFunction();
+
+// Observe DOM changes
+const observer = new MutationObserver(() => {
+    requestAnimationFrame(() => {
+        spacingFunctionDebounced();
+    });
+});
+
+
 
 (function() {
     "use strict";
 
-    // Register a keyboard shortcut (a-e) using Violentmonkey's
-    const { register } = VM.shortcut;
-    register("a-keye", () => {
-        // Run once when the script is loaded
+
+    function executeMainAction() {
         spacingFunction();
         console.log("You just pressed Shortcut in violetmonkey.");
-      // Start observing
-    if (document.readyState === "loading") {
-        document.addEventListener("DOMContentLoaded", () => {
+        // Start observing
+        if (document.readyState === "loading") {
+            document.addEventListener("DOMContentLoaded", () => {
+                observeDOM(observer);
+            });
+        } else {
             observeDOM(observer);
-        });
-    } else {
-        observeDOM(observer);
-    }
-    });
-
-    /**
-     * Adds spacing between CJK and English characters
-     */
-    function spacingFunction() {
-        const elements = document.querySelectorAll(
-            'body *:not(script):not(style):not(noscript):not(pre):not(code):not(input):not([contenteditable="true"])'
-        );
-        Array.from(elements).forEach((element) => {
-            // Only space text nodes
-            if (
-                element.childNodes.length === 1 &&
-                element.childNodes[0].nodeType === Node.TEXT_NODE
-            ) {
-                const originalText = element.textContent;
-                const newText = pangu.spacing(originalText);
-                if (originalText !== newText) {
-                    element.textContent = newText; // Update text content
-                }
-            }
-        });
+        }
     }
 
-    /**
-     * Start observing the DOM for changes
-     */
-    function observeDOM(observer) {
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true,
-        });
+
+    // Register a keyboard shortcut (a-e) using Violentmonkey's
+    const { register } = VM.shortcut;
+    register("a-keye", executeMainAction);
+
+    const autoExecuteUrlsinREGEX = GM_getValue("autoExecuteUrlsinREGEX", []);
+
+    for (let regex of autoExecuteUrlsinREGEX) {
+        console.debug("Current REGEX：", regex);
+        if (new RegExp(regex).test(window.location.href)) {
+            console.debug("Matched: ", regex);
+            executeMainAction();
+            break;
+        }
     }
-
-    /**
-     * Debounce utility function
-     */
-    function debounce(func, wait, immediate) {
-        let timeout;
-        return function() {
-            const context = this;
-            const args = arguments;
-            const later = function() {
-                timeout = null;
-                if (!immediate) {
-                    func.apply(context, args);
-                }
-            };
-            const callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) {
-                func.apply(context, args);
-            }
-        };
-    }
-
-    const spacingFunctionDebounced = debounce(spacingFunction, 300);
-
-    // Run once when the script is loaded
-    //spacingFunction();
-
-    // Observe DOM changes
-    const observer = new MutationObserver(() => {
-        requestAnimationFrame(() => {
-            spacingFunctionDebounced();
-        });
-    });
-
 
 })();
+
+
+
 
 
